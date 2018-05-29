64568cfab3b4d0879c776a61b4792c35e7c4b4fa
Samy Pallo <samypallo@samys-macbook-pro.local>
2018-05-29 16:59:01 -0400

    Initial Commit

---
+++ Samy App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Samy App
+//
+//  Created by Samy Pallo on 5/29/18.
+//  Copyright © 2018 Samy Pallo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
